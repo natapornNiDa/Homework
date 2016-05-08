df0fbab848e227b8d3733e1537467da6388a23c5
nataporn <khemsakul@gmail.com>
2016-05-08 05:03:08 +0000

    ex6 edit

--- sourceruby/exercises6.rb
+++ sourceruby/exercises6.rb
@@ -2,7 +2,7 @@
 
 # exercises 6.1
 puts "Print all the names and bank balances"
-hash = {:name =>"Polawat" ,:bank_balances => "999999999"}
+hash = {:name =>"Nataporn" ,:bank_balances => "999999999"}
 hash.each(){|key,value| puts "#{key}:#{value}"}
 puts
 
